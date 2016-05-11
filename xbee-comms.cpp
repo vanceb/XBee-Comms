6b8348e407789df561f4a13485ed547ae220986d
Vance <vance@axxe.co.uk>
2016-05-11 15:04:01 +0100

    work in progress

--- xbee-comms.cpp
+++ xbee-comms.cpp
@@ -38,18 +38,25 @@ int XBee-payload::append(uint8_t data) {
 
 int XBee-payload::append(uint16_t data) {
     if ((cursor + 2) < MAX_PAYLOAD) {
-# Big Endian conversion into payload
+        # Big Endian conversion into payload
         payload[cursor++] = (uint8_t)((data >> 8) & 0xFF);
         payload[cursor++] = (uint8_t)(data & 0xFF);
+        return 1;
+    } else {
+        return -1;
     }
 }
+
 int XBee-payload::append(uint32_t data) {
     if ((cursor + 4) < MAX_PAYLOAD) {
-# Big Endian conversion into payload
+        # Big Endian conversion into payload
         payload[cursor++] = (uint8_t)((data >> 24) & 0xFF);
         payload[cursor++] = (uint8_t)((data >> 16) & 0xFF);
         payload[cursor++] = (uint8_t)((data >> 8) & 0xFF);
         payload[cursor++] = (uint8_t)(data & 0xFF);
+        return 1;
+    } else {
+        return -1;
     }
 }
 
@@ -83,10 +90,15 @@ void XBee-appPayload::setMsgType (uint16_t value) {
 
 void XBee-appPayload::setVersion (uint16_t value) {
     version = value;
-    uint8_t cursorPos = 0;
+    uint8_t cursorPos = cursor;
     cursor = 4;
     append(value);
     cursor = cursorPos;
+}
+
+int setData(uint8_t length, uint8_t* data) {
+    cursor = 8;
+    return append(length, data);
 }
 
 uint16_t XBee-appPayload::getAppID() {
@@ -109,7 +121,193 @@ uint8_t* XBee-appPayload::getData() {
     return &payload[8];
 }
 
-XBee-comms XBee-comms::begin(Stream &serial);
-int XBee-comms::transmit(XBee-payload data);
-XBee-payload XBee-comms::receive();
+XBee-comms XBee-comms::begin(Stream &serial, int sleep_pin, int cts_pin) {
+    sleepPin = sleep_pin;
+    ctsPin = cts_pin;
+    xbee = XBee();
 
+    txStatusPayload = XBee-appPayload();
+    txStatusPayload.setAppID(TELEMETRY_APP_ID);
+    txStatusPayload.setMessageType(TX_STATUS_MSG);
+    txStatusPayload.setVersion(TX_STATUS_VERSION);
+
+    // Setup CTS pin if needed
+    if (ctsPin > 0) {
+        pinMode(cstPin INPUT);
+    }
+
+    // Make sure XBee is awake
+    wake();
+
+    xbee.begin(serial);
+}
+
+
+XBee-comms XBee-comms::begin(Stream &serial, int sleep_pin) {
+    begin(serial, sleep_pin, -1);
+}
+
+
+XBee-comms XBee-comms::begin(Stream &serial) {
+    begin(serial, -1, -1);
+}
+
+
+void XBee-comms::wake() {
+    if (sleepPin >= 0) {
+        pinMode(sleepPin, OUTPUT);
+        digitalWrite (sleepPin LOW);
+    }
+}
+
+void XBee-comms::sleep() {
+    if (sleepPin >= 0) {
+        pinMode(sleepPin, OUTPUT);
+        digitalWrite (sleepPin HIGH);
+    }
+}
+
+void XBee-comms::resetTxStatus() {
+    TxStatus.millis = millis();
+    // Tx Failures
+    TxStatus.tx_packets = 0;
+    TxStatus.tx_local_cts_timeout = 0;
+    TxStatus.tx_local_timeout = 0;
+    TxStatus.tx_cca_failure = 0;
+    TxStatus.tx_invalid_dest = 0;
+    TxStatus.tx_network_ack = 0;
+    TxStatus.tx_not_joined = 0;
+    TxStatus.tx_self_addr = 0;
+    TxStatus.tx_addr_not_found = 0;
+    TxStatus.tx_no_route = 0;
+    TxStatus.tx_payload_too_big = 0;
+    TxStatus.tx_other1 = 0;
+    TxStatus.tx_other2 = 0;
+}
+
+void sendTxStatus() {
+    now = millis();
+    if (now > nextTxStatus) {
+        TxStatus.millis = now;
+        nextTxStatus = now + TX_STATUS_UPDATE_PERIOD;
+        // check for rollover
+        if( nextTxStatus < updateTime) {
+            nextTxStatus = -1;
+        }
+        txStatusPayload.setData(TxStatus);
+        transmit(txStatusPayload);
+    }
+}
+
+
+int XBee-comms::transmit(XBee-payload data) {
+    TxStatus.tx_packets++;
+    // Make sure the XBee is awake
+    wake();
+    // Wait for CTS from the XBee or timeout
+    bool gotCTS = false;
+    if (ctsPin >= 0) {
+        long timeout = millis() + TX_CTS_TIMEOUT;
+        while(millis() < timeout) {
+            if (digitalRead(ctsPin) == false) {
+                gotCTS = true;
+                break;
+            } else {
+                delay(10);
+            }
+        }
+    } else {
+        gotCTS = true;
+    }
+
+    if(gotCTS == true){
+        // Send the Sensor data API frame to the Xbee
+        xbee.getNextFrameId();
+        
+        xbee.send(zbTx);
+        if(xbee.readPacket(TX_RESPONSE_TIMEOUT)){
+            // We got a response from our local xbee
+            if(xbee.getResponse().getApiId() == ZB_TX_STATUS_RESPONSE){
+                // This is the "all OK" route...
+                xbee.getResponse().getZBTxStatusResponse(txStatus);
+                // Update failure counters if necessary
+                switch(txStatus.getDeliveryStatus()){
+                    case SUCCESS :
+                    // Most likely route so get it out of the way first
+                    // See whether we should send a TX Status report
+                    if (millis() > nextTxUpdate) {
+
+                        fillTxStatus();
+                        xbee.getNextFrameId();
+                        xbee.send(zbTxStatus);
+                        if(xbee.readPacket(TX_RESPONSE_TIMEOUT)){
+                            // We got a response from our local xbee
+                            if(xbee.getResponse().getApiId() == ZB_TX_STATUS_RESPONSE){
+                                // This is the "all OK" route...
+                                xbee.getResponse().getZBTxStatusResponse(txStatus);
+                                if(txStatus.getDeliveryStatus() == SUCCESS) {
+                                    nextTxUpdate = millis() + TX_STATUS_UPDATE_PERIOD;
+                                }
+                            }
+                        }
+                    }
+                    return 0;
+                    break;
+                    case CCA_FAILURE :
+                    tx_cca_failure++;
+                    return -5;
+                    break;
+                    case INVALID_DESTINATION_ENDPOINT_SUCCESS :
+                    tx_invalid_dest++;
+                    return -6;
+                    break;
+                    case NETWORK_ACK_FAILURE :
+                    tx_network_ack++;
+                    return -3;
+                    break;
+                    case NOT_JOINED_TO_NETWORK :
+                    tx_not_joined++;
+                    return -4;
+                    break;
+                    case SELF_ADDRESSED :
+                    tx_self_addr++;
+                    return -7;
+                    break;
+                    case ADDRESS_NOT_FOUND :
+                    tx_addr_not_found++;
+                    return -8;
+                    break;
+                    case ROUTE_NOT_FOUND :
+                    tx_no_route++;
+                    return -9;
+                    break;
+                    case PAYLOAD_TOO_LARGE :
+                    tx_payload_too_big++;
+                    return -10;
+                    break;
+                    default :
+                    // Unexpected error
+                    tx_other1++;
+                    return -11;
+                }
+                return txStatus.getDeliveryStatus();
+            } else {
+                // There was another recieve packet or modem status???
+                // Increment the failure counter
+                tx_other2++;
+                return -12;
+            }
+        } else {
+            //Timed out waiting for a response from the XBee about the TX Status
+            // Increment the failure counter
+            tx_local_timeout++;
+            return -2;
+        }
+    } else {
+        // We didn't get CTS from the local XBee within the timeout
+        // Update the failure counter
+        tx_local_cts_timeout++;
+        return -1;
+    }
+}
+XBee-payload XBee-comms::receive();
--- xbee-comms.h
+++ xbee-comms.h
@@ -5,55 +5,105 @@
 
 #define MAX_PAYLOAD 100
 
+#define TELEMETRY_APP_ID 0x0573
+#define TX_STATUS_MSG 0x0001
+#define TX_STATUS_VERSION 0x0001
+#define TX_STATUS_UPDATE_PERIOD 60000
+
+#define TX_CTS_TIMEOUT 1000
+#define TX_RESPONSE_TIMEOUT 5000
+
 class XBee-payload {
-    private:
-        uint8_t payload[MAX_PAYLOAD];
-        uint8_t cursor;
+private:
+    uint8_t payload[MAX_PAYLOAD];
+    uint8_t cursor;
 
-    protected:
+protected:
 
-    public:
-        void clear();
-        int set(uint8_t size, uint8_t* data);
-        int append(uint8_t size, uint8_t* data);
-        int append(uint8_t data);
-        int append(uint16_t data);
-        int append(uint32_t data);
+public:
+    void clear();
+    int set(uint8_t size, uint8_t* data);
+    int append(uint8_t size, uint8_t* data);
+    int append(uint8_t data);
+    int append(uint16_t data);
+    int append(uint32_t data);
 
-        uint8_t* getPayload();
-        uint8_t getPayloadSize();
+    uint8_t* getPayload();
+    uint8_t getPayloadSize();
 }
+
 
 class XBee-appPayload: Public XBee-payload {
-    private:
-        uint16_t appID;
-        uint16_t msgType;
-        uint16_t version;
-        uint16_t length;
+private:
+    uint16_t appID;
+    uint16_t msgType;
+    uint16_t version;
+    uint16_t length;
 
-    protected:
+protected:
 
-    public:
-        void setAppID(uint16_t value);
-        void setMsgType (uint16_t value);
-        void setVersion (uint16_t value);
-        uint16_t getAppID();
-        uint16_t getMsgType();
-        uint16_t getVersion();
-        uint8_t getDataLength();
-        uint8_t* getData();
+public:
+    void setAppID(uint16_t value);
+    void setMsgType (uint16_t value);
+    void setVersion (uint16_t value);
+    int setData(uinta_t length, uint8_t* data);
+    uint16_t getAppID();
+    uint16_t getMsgType();
+    uint16_t getVersion();
+    uint8_t getDataLength();
+    uint8_t* getData();
 
 }
+
+
 class XBee-comms {
-    private:
-        XBee xbee;
+private:
+    static XBee xbee;
+    static int sleepPin;
+    static int ctsPin;
 
-    protected:
+    unsigned long nextTxStatus;
 
-    public:
-        XBee-comms begin(Stream &serial);
-        int transmit(XBee-payload data);
-        XBee-payload receive();
+    // Create a payload item for our TxStatus updates
+    XBee-appPayload txStatusPayload;
+
+    // Define a sctructure to hold the TX Status data
+    typedef struct tagTxStatus {
+        // Time since restart
+        uint32_t millis;
+        // Tx Failures
+        uint16_t tx_packets;
+        uint16_t tx_local_cts_timeout;
+        uint16_t tx_local_timeout;
+        uint16_t tx_cca_failure;
+        uint16_t tx_invalid_dest;
+        uint16_t tx_network_ack;
+        uint16_t tx_not_joined;
+        uint16_t tx_self_addr;
+        uint16_t tx_addr_not_found;
+        uint16_t tx_no_route;
+        uint16_t tx_payload_too_big;
+        uint16_t tx_other1;
+        uint16_t tx_other2;
+    } TxStatus;
+
+protected:
+    sleep();
+    wake();
+    resetTxStatus();
+    sendTxStatus();
+
+public:
+    XBee-comms begin(Stream &serial, int sleep_pin, int cts_pin);
+    XBee-comms begin(Stream &serial, int sleep_pin);
+    XBee-comms begin(Stream &serial);
+
+    int transmit(XBee-payload data);
+    XBee-payload receive();
 
 }
+
+
+
+
 #endif
